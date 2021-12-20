c6c193ae59dfe5ee5ab2e6dfcbad2f4e2fa40052
Francis Rubio <francisrubio@protonmail.com>
2021-12-20 22:58:59 +0800

    Save user responses

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -10,38 +10,56 @@ $$(".button").forEach((button) => {
   });
 });
 
-$$('.cart').forEach(async cart => {
+$$(".cart").forEach(async (cart) => {
   const products = await Inventory.fetchProducts();
-  window.shoppingCart = new ShoppingCart(cart, products)
-  
-  console.log({ shoppingCart })
-})
+  window.shoppingCart = new ShoppingCart(cart, products);
+
+  console.log({ shoppingCart });
+});
 
 $$("[data-product-grid]").forEach((section) => {
   section.addEventListener("input", (event) => {
-    console.log(event);
-
     if (event.target.matches("[data-update-cart]")) {
-      const input = event.target
-      const quantity = input.value
-      const productID = input.dataset.updateCart
-
-      if (quantity > 10 || quantity < 0) {
-        alert("Maximum of 10 items per product only")
-        return
-      }
-
-      if (quantity && productID) {
-        shoppingCart.addItem({ id: productID, quantity })
-
-        if (quantity > 0) {
-          input.closest(".card").classList.add("selected");
-          input.closest(".card").dataset.quantity = quantity;
-        } else {
-          input.closest(".card").classList.remove("selected")
-          delete input.closest(".card").dataset.quantity
-        }
-      }
+      const input = event.target;
+      handleEvent(input);
     }
   });
 });
+
+setTimeout(() => {
+  // Pre-populate cart with saved items from previous session
+  // via localStorage
+  $$("[data-product-card]").forEach((card) => {
+    const id = card.id;
+    const input = card.querySelector("input[data-update-cart]");
+
+    if (input) {
+      input.value = localStorage.getItem(id);
+      handleEvent(input);
+    }
+  });
+}, 1000)
+
+function handleEvent(input) {
+  const quantity = input.value;
+  const productID = input.dataset.updateCart;
+
+  if (quantity > 10 || quantity < 0) {
+    alert("Maximum of 10 items per product only");
+    return;
+  }
+
+  if (quantity && productID) {
+    window.shoppingCart.addItem({ id: productID, quantity });
+
+    if (quantity > 0) {
+      input.closest(".card").classList.add("selected");
+      input.closest(".card").dataset.quantity = quantity;
+      localStorage.setItem(productID, quantity);
+    } else {
+      input.closest(".card").classList.remove("selected");
+      delete input.closest(".card").dataset.quantity;
+      localStorage.removeItem(productID);
+    }
+  }
+}
